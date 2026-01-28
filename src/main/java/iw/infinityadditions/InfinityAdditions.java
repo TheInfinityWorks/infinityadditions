ec70d96cfb1e8274107f3a27fd558347776a1f08
marcus <marcus.silva@eaportal.org>
2026-01-28 16:31:25 -0300

    feat: Ruby Block finished and implementation of Cactus Fiber completed

--- src/main/java/iw/infinityadditions/InfinityAdditions.java
+++ src/main/java/iw/infinityadditions/InfinityAdditions.java
@@ -5,6 +5,9 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static iw.infinityadditions.block.ModBlocks.registerModBlocks;
+import static iw.infinityadditions.item.ModItems.registerModItems;
+
 public class InfinityAdditions implements ModInitializer {
 	public static final String MOD_ID = "infinity-additions";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -12,5 +15,7 @@ public class InfinityAdditions implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing Mod!");
+        registerModItems();
+        registerModBlocks();
 	}
 }
---
+++ src/main/java/iw/infinityadditions/block/ModBlocks.java
@@ -0,0 +1,52 @@
+package iw.infinityadditions.block;
+
+import iw.infinityadditions.InfinityAdditions;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+//    private static Block registerBlock(String name, Block block){
+//        registerBlockItem(name, block);
+//        return Registry.register(Registries.BLOCK, Identifier.of(InfinityAdditions.MOD_ID, name), block);
+//    }
+//
+//    private static void registerBlockItem(String name, Block block){
+//        // return Registry.register(Registries.ITEM, Identifier.of(InfinityAdditions.MOD_ID, name), item);
+//        Registry.register(Registries.ITEM, Identifier.of(InfinityAdditions.MOD_ID, name),
+//                new BlockItem(block, new Item.Settings()));
+//    }
+
+    private static Block registerBlock(String name, AbstractBlock.Settings blockSettings) {
+        RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(InfinityAdditions.MOD_ID, name));
+        Block block = new Block(blockSettings.registryKey(key));
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, key, block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(InfinityAdditions.MOD_ID, name));
+        BlockItem item = new BlockItem(block, new Item.Settings().registryKey(key));
+        Registry.register(Registries.ITEM, key, item);
+    }
+
+
+    public static final Block RUBY_BLOCK = registerBlock("ruby_block", AbstractBlock.Settings.create().strength(4f, 4f).requiresTool().sounds(BlockSoundGroup.METAL));
+
+    public static void registerModBlocks(){
+        InfinityAdditions.LOGGER.info("Registering Infinity Additions Mod Blocks!");
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(RUBY_BLOCK);
+        });
+    }
+}
